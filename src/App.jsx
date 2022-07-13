91fffa1fc88be18cfab972c6d46eebf098adf7f9
LuizFylip <54417643+fymorGod@users.noreply.github.com>
2022-07-13 17:25:27 -0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { NavBar, Banner} from "./components/NavBar";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Banner/>
+    </div>
+  );
+}
+
+export default App;
