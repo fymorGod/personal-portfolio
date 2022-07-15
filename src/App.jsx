a4d762f4c7e16560029c1a8756e99e73501cb769
LuizFylip <54417643+fymorGod@users.noreply.github.com>
2022-07-15 14:52:56 -0300

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { NavBar, Banner} from "./components/NavBar";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Banner/>
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
